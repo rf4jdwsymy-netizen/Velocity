d9acceb4889d6fe3fd3f01f0c061e36569f452f3
desudoru <rf4jdwsymy@privaterelay.appleid.com>
2026-07-27 03:09:35 +0900

    Update velocity hub.lua

--- velocity hub.lua
+++ velocity hub.lua
@@ -736,7 +736,7 @@ function OrionLib:MakeWindow(WindowConfig)
 		AddThemeObject(MakeElement("Stroke"), "Stroke")
 	})
 
-	local MainWindow = SetChildren(SetProps(MakeElement("RoundImageFrame", Color3.fromRGB(255, 255, 255), 0, 10, "rbxassetid://131430093210340"), {
+	local MainWindow = SetChildren(SetProps(MakeElement("RoundImageFrame", Color3.fromRGB(255, 255, 255), 0, 10, "rbxassetid://129099363794409"), {
 		Parent = Orion,
 		Position = UDim2.new(0.5, -307, 0.5, -172),
 		Size = UDim2.new(0, 615, 0, 344),
