eaddfce9a0d4bc5f19f741e55ca2bf23b337bc66
desudoru <rf4jdwsymy@privaterelay.appleid.com>
2026-08-03 23:41:54 +0900

    Update velocity hub.lua

--- velocity hub.lua
+++ velocity hub.lua
@@ -693,8 +693,8 @@ function OrionLib:MakeWindow(WindowConfig)
 	local MainWindow = AddThemeObject(SetChildren(SetProps(MakeElement("RoundFrame", Color3.fromRGB(255, 255, 255), 0, 10), {
 		Parent = Orion,
 		Position = UDim2.new(0.5, -307, 0.5, -172),
-		Size = UDim2.new(0, 600, 0, 200),
-		BackgroundTransparency = 0.1,
+		Size = UDim2.new(0, 650, 0, 400),
+		BackgroundTransparency = 0.4,
 		ClipsDescendants = true
 	}), {
 		--SetProps(MakeElement("Image", "rbxassetid://3523728077"), {
